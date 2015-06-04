476e8ae0bb0299acc3fd3e94c44710bafda565de
rolies <azheema19@hotmail.com>
2015-06-05 00:49:10 +0700

    adding first folder from OOP class pracctice

---
+++ GUIapp/src/implementation/koneksiMysql.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementation;
+
+import java.sql.*;
+
+/**
+ *
+ * @author bima
+ */
+public class koneksiMysql {
+     
+    String url,usr,pwd,dbn;
+    public koneksiMysql(String dbn){
+        this.url = "jdbc:mysql://localhost/"+dbn;
+        this.usr = "root";
+        this.pwd = "";
+    }
+    public koneksiMysql(String host,String user, String pass,String dbn){
+        this.url = "jdbc:mysql://"+host+"/"+dbn;
+        this.usr = user;
+        this.pwd = pass;
+    }
+    public Connection getConnection(){
+        Connection con = null;
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(this.url,this.usr,this.pwd);
+    }catch(ClassNotFoundException e){
+        System.out.println("Error %1 : "+e.getMessage());
+        System.exit(0);
+    }catch(SQLException e){
+        System.out.println("Error %2 : "+e.getMessage());
+        System.exit(0);
+    }
+    return con;
+    }
+}
