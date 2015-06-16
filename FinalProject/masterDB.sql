463836e6d180ce1be033e2802fa9ac1ee59344ca
rolies <azheema19@hotmail.com>
2015-06-16 07:00:14 +0700

    take SQL from ubuntu

--- FinalProject/masterDB.sql
+++ FinalProject/masterDB.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.11
+-- version 4.4.3
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 11, 2015 at 04:56 PM
+-- Host: localhost
+-- Generation Time: Jun 16, 2015 at 01:58 
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -17,16 +17,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `masterdb`
+-- Database: `masterDB`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `listbus`
+-- Table structure for table `listBus`
 --
 
-CREATE TABLE IF NOT EXISTS `listbus` (
+CREATE TABLE IF NOT EXISTS `listBus` (
   `kode` varchar(10) NOT NULL,
   `nm_bus` varchar(20) NOT NULL,
   `jmlh_Bus` int(5) NOT NULL,
@@ -34,23 +34,22 @@ CREATE TABLE IF NOT EXISTS `listbus` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `listbus`
+-- Dumping data for table `listBus`
 --
 
-INSERT INTO `listbus` (`kode`, `nm_bus`, `jmlh_Bus`, `jmlh_Bangku`) VALUES
-('01ECO', 'Sumber Kencono', 2, 20),
-('01VIP', 'Sumber Kencono', 4, 30),
+INSERT INTO `listBus` (`kode`, `nm_bus`, `jmlh_Bus`, `jmlh_Bangku`) VALUES
+('01ECO', 'Gumarang', 2, 20),
+('01VIP', 'Gumarang', 4, 30),
 ('02AC', 'Gumarang', 8, 30),
-('02PTS', 'Gumarang', 4, 40),
-('02VIP', 'Gumarang', 91, 34);
+('02PTS', 'Gumarang', 4, 40);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `listperjalanan`
+-- Table structure for table `listPerjalanan`
 --
 
-CREATE TABLE IF NOT EXISTS `listperjalanan` (
+CREATE TABLE IF NOT EXISTS `listPerjalanan` (
   `kd_prjlnan` varchar(10) NOT NULL,
   `ID_bus` varchar(20) NOT NULL,
   `region` varchar(15) NOT NULL,
@@ -60,24 +59,34 @@ CREATE TABLE IF NOT EXISTS `listperjalanan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `listperjalanan`
+-- Dumping data for table `listPerjalanan`
 --
 
-INSERT INTO `listperjalanan` (`kd_prjlnan`, `ID_bus`, `region`, `waktu`, `tujuan`, `harga`) VALUES
-('001', '01VIP', 'Jawa', 'Siang', 'Madiun', 60000),
-('002', '02PTS', 'Sumatra', 'Malam', 'Palembang', 200000),
-('003', '02AC', 'Sumatra', 'Siang', 'Lampung', 300000),
-('004', '02AC', 'Kalimantan', 'Siang', 'Sampit', 300000),
-('005', '01VIP', 'Sulawesi', 'Siang', 'Makassar', 500000),
-('006', '02PTS', 'Bali & Nt', 'Malam', 'Lombok', 450000);
+INSERT INTO `listPerjalanan` (`kd_prjlnan`, `ID_bus`, `region`, `waktu`, `tujuan`, `harga`) VALUES
+('001', '01ECO', 'Jawa', 'Siang', 'Jakarta', 200000),
+('002', '01VIP', 'Jawa', 'Malam', 'Jakarta', 300000),
+('003', '02AC', 'Jawa', 'Siang', 'Jakarta', 400000),
+('004', '02PTS', 'Jawa', 'Siang', 'Jakarta', 300000),
+('005', '01ECO', 'Sumatra', 'Siang', 'Riau', 250000),
+('006', '01VIP', 'Sumatra', 'Malam', 'Riau', 400000),
+('007', '02AC', 'Sumatra', 'Siang', 'Riau', 500000),
+('008', '02PTS', 'Sumatra', 'Malam', 'Riau', 580000),
+('009', '01ECO', 'Kalimantan', 'Siang', 'Pontianak', 400000),
+('010', '01VIP', 'Kalimantan', 'Siang', 'Pontianak', 480000),
+('011', '02AC', 'Kalimantan', 'Siang', 'Pontianak', 580000),
+('012', '02PTS', 'Kalimantan', 'Siang', 'Pontianak', 680000),
+('013', '01ECO', 'Sulawesi', 'Siang', 'Makassar', 580000),
+('014', '01VIP', 'Sulawesi', 'Makassar', 'Malam', 680000),
+('015', '02AC', 'Sulawesi', 'Siang', 'Makassar', 730000),
+('016', '02PTS', 'Sulawesi', 'Siang', 'Makassar', 789000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `listtransaksi`
+-- Table structure for table `listTransaksi`
 --
 
-CREATE TABLE IF NOT EXISTS `listtransaksi` (
+CREATE TABLE IF NOT EXISTS `listTransaksi` (
   `ID_trans` varchar(10) NOT NULL,
   `waktu` varchar(10) NOT NULL,
   `nm_customer` varchar(20) NOT NULL,
@@ -86,19 +95,22 @@ CREATE TABLE IF NOT EXISTS `listtransaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `listtransaksi`
+-- Dumping data for table `listTransaksi`
 --
 
-INSERT INTO `listtransaksi` (`ID_trans`, `waktu`, `nm_customer`, `contact`, `total`) VALUES
-('1', '2015-6- 11', 'rolies', '085330', 300000);
+INSERT INTO `listTransaksi` (`ID_trans`, `waktu`, `nm_customer`, `contact`, `total`) VALUES
+('1', '2015-6- 11', 'rolies', '085330', 300000),
+('2', '2015-6- 12', 'Makhi', '085330793203', 300000),
+('3', '2015-6- 15', 'Faisyal', '085330793203', 300000),
+('4', '2015-6- 15', 'Jarib', '123', 300000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `listtransaksidetail`
+-- Table structure for table `listTransaksiDetail`
 --
 
-CREATE TABLE IF NOT EXISTS `listtransaksidetail` (
+CREATE TABLE IF NOT EXISTS `listTransaksiDetail` (
   `ID_trans` varchar(10) NOT NULL,
   `ID_route` varchar(10) NOT NULL,
   `no_bgku` int(3) NOT NULL,
@@ -106,11 +118,13 @@ CREATE TABLE IF NOT EXISTS `listtransaksidetail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `listtransaksidetail`
+-- Dumping data for table `listTransaksiDetail`
 --
 
-INSERT INTO `listtransaksidetail` (`ID_trans`, `ID_route`, `no_bgku`, `harga`) VALUES
-('1', '003', 3, 300000);
+INSERT INTO `listTransaksiDetail` (`ID_trans`, `ID_route`, `no_bgku`, `harga`) VALUES
+('1', '003', 3, 300000),
+('2', '003', 3, 300000),
+('3', '006', 3, 400000);
 
 -- --------------------------------------------------------
 
@@ -137,15 +151,15 @@ INSERT INTO `login` (`id`, `user_id`, `name`, `password`) VALUES
 --
 
 --
--- Indexes for table `listbus`
+-- Indexes for table `listBus`
 --
-ALTER TABLE `listbus`
+ALTER TABLE `listBus`
   ADD PRIMARY KEY (`kode`);
 
 --
--- Indexes for table `listtransaksi`
+-- Indexes for table `listTransaksi`
 --
-ALTER TABLE `listtransaksi`
+ALTER TABLE `listTransaksi`
   ADD PRIMARY KEY (`ID_trans`);
 
 --
